1358320f548393f6ab3eaaf87be8df1f2091183b
Kim <kimberlyntellez@gmail.com>
2022-09-11 16:37:08 -0500

    Nav bar updated for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './css/navbar.css';
 import logo from "./img/logo.png";
 
-function Navbar() {
+export default function Navbar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
@@ -11,69 +11,62 @@ function Navbar() {
 
   return (
     <>
-      <nav className='navbar nav-nav sticky-top navbar-expand-lg navbar-light'>
-        <div className="container-fluid">
+      <nav className='navbar sticky-top navbar-expand-lg navbar-light'>
+        <div className='container-fluid'>
           <div className='container'>
-            <div className="d-flex justify-content-center">
-              <NavLink className="navbar-brand" to={"/ux-ui-portfolio"}>
-                Kim T&eacute;llez | UX/UI Design Portfolio 
-                  </NavLink>
-              </div>
-            
+            <NavLink to={'/ux-ui-portfolio/'} className='navbar-logo' onClick={closeMobileMenu}>
+              Kim Téllez | UX/UI <br className='head-break' />Design Portfolio
+            </NavLink>
 
             <div className="d-none d-lg-block d-lg-flex justify-content-center align-items-center">
-              <a className="navbar-brand nav-large m-auto" href={"/ux-ui-portfolio"}>
+              <a href={"/ux-ui-portfolio"}>
                 <img alt="Site Logo" src={logo} />
               </a>
             </div>
-          
+
             <div className='menu-icon' onClick={handleClick}>
-              <div className={click ? '' : 'fa-box'}><i className={click ? 'fas fa-times' : 'fas fa-bars fa-box'} /></div>
+              <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
-            <ul className={click ? 'nav-menu active' : 'navbar-nav nav-menu'}>
-              <div className="d-block  d-lg-none m-auto"><img className="img-fluid" alt="Site Logo" src={logo} /></div>
-              <li className='nav-item pr-3'>
-                  <NavLink
-                    to={"/ux-ui-portfolio"}
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <div className="d-block  d-lg-none m-auto pb-3"><img className="img-fluid" alt="Site Logo" src={logo} /></div>
+              <li className='nav-item'>
+                <NavLink to={'/ux-ui-portfolio/'} className='nav-link' activeClassName='active' onClick={closeMobileMenu}>
+                  HOME
+                </NavLink>
+              </li>
+
+              <li className="nav-item dropdown d-none d-lg-block">
+                <span className="nav-link dropdown-toggle" activeClassName='active' role="button" data-toggle="dropdown" aria-expanded="false">
+                  CASE STUDIES
+                </span>
+                <div className="dropdown-menu">
+                  <NavLink to={'/ux-ui-portfolio/case-studies/vauven'} className='dropdown-item' onClick={closeMobileMenu}>Vauven</NavLink>
+                  <NavLink to={'/ux-ui-portfolio/case-studies/haven-station'} className='dropdown-item' onClick={closeMobileMenu}>Haven Station</NavLink>
+                  <NavLink to={'/ux-ui-portfolio/case-studies/bpmusic'} className='dropdown-item' onClick={closeMobileMenu}>BPMusic</NavLink>
+                  <NavLink to={'/ux-ui-portfolio/case-studies/readingful-moments'} className='dropdown-item' onClick={closeMobileMenu}>Readingful Moments</NavLink>
+                </div>
+              </li>
+
+              <li className="nav-item dropdown d-lg-none d-block">
+                <NavLink className="nav-link" activeClassName='active' to={"/ux-ui-portfolio/case-studies/vauven"}>
+                  CASE STUDIES
+                </NavLink>
+                <ul>
+                  <li><NavLink to={'/ux-ui-portfolio/case-studies/vauven'} className='nav-mini' activeClassName='active' onClick={closeMobileMenu}>Vauven</NavLink></li>
+                  <li><NavLink to={'/ux-ui-portfolio/case-studies/haven-station'} className='nav-mini' activeClassName='active' onClick={closeMobileMenu}>Haven Station</NavLink></li>
+                  <li><NavLink to={'/ux-ui-portfolio/case-studies/bpmusic'} className='nav-mini' activeClassName='active' onClick={closeMobileMenu}>BPMusic</NavLink></li>
+                  <li><NavLink to={'/ux-ui-portfolio/case-studies/readingful-moments'} className='nav-mini' activeClassName='active' onClick={closeMobileMenu}>Readingful Moments</NavLink></li>
+                </ul>
+              </li>
+
+              <li className='nav-item'>
+                <a target="_blank" 
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/kimberly-tellez/"
                     className='nav-link'
                     activeClassName="active"
                     onClick={closeMobileMenu}
                   >
-                    HOME
-                  </NavLink>
-              </li>
-              
-              <li className="nav-item dropdown pr-3">
-                <div className='d-none d-lg-block'>
-                    <button type='button' className="nav-link btn-clr dropdown-toggle btn" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        CASE STUDIES
-                    </button>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" >
-                        <NavLink activeClassName="active" className="dropdown-item" to={"/ux-ui-portfolio/case-studies/vauven"} >Vauven</NavLink>
-                        <NavLink activeClassName="active" className="dropdown-item" to={"/ux-ui-portfolio/case-studies/haven-station"} >Haven Station</NavLink>
-                        <NavLink activeClassName="active" className="dropdown-item" to={"/ux-ui-portfolio/case-studies/bpmusic"} >BPMusic</NavLink>
-                        <NavLink activeClassName="active" className="dropdown-item" to={"/ux-ui-portfolio/case-studies/readingful-moments"} >Readingful Moments</NavLink>
-                    </div>
-                </div>
-
-                <div className='dd d-inline d-lg-none'>
-                    <ul>CASE STUDIES
-                        <li><NavLink className={"inline-link"} activeClassName="active" to={"/case-studies/vauven"} onClick={closeMobileMenu}>Vauven</NavLink></li>
-                        <li><NavLink className={"inline-link"} activeClassName="active" to={"/case-studies/haven-station"} onClick={closeMobileMenu}>Haven Station</NavLink></li>
-                        <li><NavLink className={"inline-link"} activeClassName="active" to={"/case-studies/bpmusic"} onClick={closeMobileMenu}>BPMusic</NavLink></li>
-                        <li><NavLink className={"inline-link"} activeClassName="active" to={"/case-studies/readingful-moments"} onClick={closeMobileMenu}>Readingful Moments</NavLink></li>
-                    </ul>
-                </div>
-              </li>
-
-              <li className='nav-item pr-lg-5'>
-                <a target="_blank" 
-                  rel="noreferrer"
-                  href="https://www.linkedin.com/in/kimberly-tellez/"
-                  className='nav-link'
-                  activeClassName="active"
-                  onClick={closeMobileMenu}
-                >
                   LINKEDIN
                 </a>
               </li>
@@ -84,5 +77,3 @@ function Navbar() {
     </>
   );
 }
-
-export default Navbar;
